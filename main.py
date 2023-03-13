cb06b1ac353273494b71cebe128e9ece90ffea07
Lorran <59341592+lorranprt@users.noreply.github.com>
2023-03-13 17:50:29 +0000

    main code is changed for a log calculator

--- main.py
+++ main.py
@@ -1,54 +1,21 @@
-import random
-
-# Define the size of the game board
-ROWS = 5
-COLS = 5
-
-# Define the number of mines
-MINES = 3
-
-# Create the game board
-board = [[0 for j in range(COLS)] for i in range(ROWS)]
-
-# Place the mines randomly on the board
-for i in range(MINES):
-  row = random.randint(0, ROWS - 1)
-  col = random.randint(0, COLS - 1)
-  board[row][col] = "*"
+import math
 
 
-# Define a function to print the board
-def print_board(board):
-  for row in board:
-    print(" ".join(str(cell) for cell in row))
+def log(base, number):
+
+  if base <= 0 or number <= 0:
+    return "Invalid input: both base and number must be greater than 0"
+  if base == 1:
+    return "Invalid input: base cannot be 1"
+
+  result = math.log(number, base)
+  return result
 
 
-# Define a function to get the user's guess
-def get_guess():
-  row = int(input("Guess row (0-4): "))
-  col = int(input("Guess col (0-4): "))
-  return row, col
+print(log(10, 1000))
+print(log(2, 8))
+print(log(3, 81))
+print(log(1, 5))
+print(log(-2, 10))
 
-
-# Define a function to check if the guess is correct
-def is_correct_guess(row, col):
-  if board[row][col] == "*":
-    print("You hit a mine!")
-    return True
-  else:
-    print("You missed!")
-    return False
-
-
-# Play the game
-print("Welcome to Minefield!")
-print("There are", MINES, "mines hidden on the board.")
-print_board(board)
-
-while True:
-  row, col = get_guess()
-  if is_correct_guess(row, col):
-    break
-
-print_board(board)
-print("Game over!")
+# importar pro git, quero brincar mais
---
+++ test.md
@@ -0,0 +1,6 @@
+a = float(input("Digite a altura do triângulo: "))
+b = float(input("Digite a base do triângulo: "))
+
+area = (b * a) / 2
+
+print("A área do triângulo é:", area)
